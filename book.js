3c3c15fd1a585e856e85624860582100ee34259e
sondr0p <psndnb@gmail.com>
2019-01-04 04:11:10 -0800

    can add books to table

--- book.js
+++ book.js
@@ -1,3 +1,6 @@
+let myLibrary = [];
+let numCol = 4;
+
 function book (title, author, pages, haveRead){
     this.title = title;
     this.author = author;
@@ -6,15 +9,42 @@ function book (title, author, pages, haveRead){
 
     this.info = function() {
         if(haveRead) {
-            console.log(title + " by " + author + ", " + pages + " pages, have read");
+            // console.log(title + " by " + author + ", " + pages + " pages, have read");
+            return "" + title + " by " + author + ", " + pages + " pages, have read";
         }else{
-            console.log(title + " by " + author + ", " + pages + " pages, have not read");
+            // console.log(title + " by " + author + ", " + pages + " pages, have not read");
+            return "" + title + " by " + author + ", " + pages + " pages, have not read";
         }
         
     }
 }
 
-b = new book("harry potter", "jk rowling", 300, true);
-c = new book("cat in a hat", "dr seuss", 50, false);
-b.info();
-c.info();
+// Loops through array and displays each book on page
+function render() {
+    var row = document.createElement("tr");
+    for(i = 0; i < myLibrary.length; i++){
+        var title = document.createElement("td");
+        var author = document.createElement("td");
+        var pages = document.createElement("td");
+        var ifRead = document.createElement("td");
+        title.innerHTML = myLibrary[i].title;
+        author.innerHTML = myLibrary[i].author;
+        pages.innerHTML = myLibrary[i].pages;
+        ifRead.innerHTML = myLibrary[i].haveRead;
+        row.appendChild(title);
+        row.appendChild(author);
+        row.appendChild(pages);
+        row.appendChild(ifRead);
+    }
+    document.getElementById("table").appendChild(row);
+}
+
+function addBook(){
+    var t = prompt("Please enter a title");
+    var a = prompt("Please enter an author");
+    var p = prompt("Please enter number of pages");
+    var ifRead = prompt("Please enter if read (true/false)");
+    var b = new book(t, a, p, ifRead);
+    myLibrary.push(b);
+    render();
+}
